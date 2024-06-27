68b0c05b815deb97928e058deb29999b7bf55501
MelaniRuvalcaba <melanilizethgomezruvalcaba@gmail.com>
2024-06-27 17:31:04 -0600

    feature: modelo creado

--- models/food.models.js
+++ models/food.models.js
@@ -1,1 +1,31 @@
-console.log("hello")
+import mongoose from "mongoose";
+const {Schema, model} = mongoose;
+
+const servicioSchema = new Schema({
+    name:
+    {
+        type:String,
+        require:true
+    },
+    ingredients:
+    {
+        type:String,
+        require:true
+    },
+    cost:
+    {
+        type:Number,
+        require:true
+    },
+    description:
+    {
+        type:String,
+        require:true
+    }
+   
+    
+},{
+    timestamps: true
+});
+
+export const servicioModel = model('solicitud', servicioSchema);
