e34e87f667e0781e4b9a3443800c6e193ed8c0d7
Fernanda Perez <2136000535@utna.edu.mx>
2024-06-28 20:34:47 -0600

    Creacion del los demas Endopoints query, delete and update

--- controllers/food.controllers.js
+++ controllers/food.controllers.js
@@ -6,7 +6,7 @@ const {messageGeneral} = message;
 const foodCtrl = {};
 
 //New register food
-foodCtrl.createFood = async(req,res)=>{
+foodCtrl.ingresafood = async(req,res)=>{
     try {
         const data = req.body;
         const resp = await foodModel.create(data);
@@ -17,3 +17,57 @@ foodCtrl.createFood = async(req,res)=>{
 };
 
 
+//Consult all records
+foodCtrl.mostrarTodo = async(req,res)=>{
+    try {
+        const resp = await foodModel.find().populate({
+            path: "folio"})
+            messageGeneral(res,)
+    } catch (error) {
+        messageGeneral(res,500,false,"",error.message);
+    }
+}
+
+//Update 
+foodCtrl.editarVino = async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const resp = await foodModel.findById(id);
+        if(!resp){
+            return messageGeneral(res,404,false,"","Registro no encontrada");
+        }
+        await resp.updateOne(req.body);
+        messageGeneral(res,200,true,"","Registro actualizada");
+    } catch (error) {
+        messageGeneral(res,500,false,"",error.message);
+    }
+}
+
+//Delete
+foodCtrl.eliminarVino = async(req,res) =>{
+    try {
+        const { id } = req.params;
+        const resp = await foodModel.findById(id);
+        if(!resp){
+            return messageGeneral(res,404,false,"","Registro no encontrado");
+        }
+        await resp.deleteOne();
+        messageGeneral(res,200,true,"","Registro eliminado");
+    } catch (error) {
+        messageGeneral(res,500,false,"",error.message);
+    }
+};
+
+//Consult by product name
+foodCtrl.listByName = async(req,res) => {
+    try {
+        const { name } = req.params;
+        const resp = await foodModel.findOne({ name });
+        if(!resp){
+            return messageGeneral(res,404,false,"","Registro no encontrado");
+        }
+        messageGeneral(res,200,true,resp,"Registro encontrado")
+    } catch (error) {
+        messageGeneral(res,500,false,"",error.message);
+    }
+}
