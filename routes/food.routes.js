4c51c6907ad935e0cc463725f616ffd476e5a0cf
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-06-30 10:56:10 -0600

    Refactor: endpoints improved; endpoints name changed

--- routes/food.routes.js
+++ routes/food.routes.js
@@ -3,10 +3,16 @@ const controller = require("../controllers/food.controllers.js");
 
 const router = express.Router();
 
-router.get("/food", controller.listDishes)
-    .get("/food/:name", controller.getDish)
-    .post("/food", controller.newDish)
-    .put("/food/:name", controller.updateDish)
-    .delete("/food/:name", controller.deleteDish);
+router.get("/", controller.listDishes);
+router.get("/get", controller.getDish);
+router.post("/new", controller.newDish);
+router.put("/update", controller.updateDish);
+router.delete("/delete", controller.deleteDish);
+
+// == == == THIS IS CALLED WHITE LIST == == ==
+router.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
+// == == == == == == == == == == == == == == ==
 
 module.exports = router;
