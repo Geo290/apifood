54d91247af4261d5b511d197e23c83280debfc32
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-06-30 00:16:49 -0600

    Fix: changed mongodb dependency to mongoose

--- config/db.js
+++ config/db.js
@@ -1,28 +1,33 @@
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 
 const USER = process.env.DB_USER;
 const PASS = process.env.DB_PASSKEY;
 const DB = process.env.DB_NAME;
-const COLLECTION = process.env.DB_COLLECTION;
+// const COLLECTION = process.env.DB_COLLECTION;
 
-const uri = `mongodb+srv://${USER}:${PASS}@cluster0.8puqbr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const URI = `mongodb+srv://${USER}:${PASS}@cluster0.8puqbr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
 const run = async () => {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await mongoose.connect(URI, {
+      useNewUrlParser : true,
+      useUnifiedTopology: true,
+      dbName: 'apifood',
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db(`${DB}`).command({ ping: 1 });
+    // await client.db(`${DB}`).command({ ping: 1 });
     console.log("App successfully connected to DB");
 
   } catch (error) {
@@ -33,18 +38,16 @@ const run = async () => {
 };
 
 const stop = async (server) => {
-
   server.close(async (error) => {
     if (error) {
       console.error("Error while closing the server: ", error);
       process.exit(1);
     };
-
   });
   
   try {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await mongoose.disconnect()
     console.log("App successfully disconnected from DB");
     process.exit(0);
 
