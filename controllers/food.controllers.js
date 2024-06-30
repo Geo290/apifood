77bf6ebdb6fd670f0ebb45efb7c775583982356c
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-06-29 21:35:04 -0600

    - Fix: Import & export way changed to commonjs - Fix: Controllers name changed - Fix: Main parameter for search in DB changed to 'name'

--- controllers/food.controllers.js
+++ controllers/food.controllers.js
@@ -1,73 +1,76 @@
 //Importar el archivo del Model
-import {foodModel} from "../models/food.models.js"
-import message from "../utils/messages.js";
+const { foodModel } = require("../models/food.models.js");
+const { message } = require("../utils/messages.js");
 
-const {messageGeneral} = message;
 const foodCtrl = {};
+const { messageGeneral } = message;
 
 //New register food
-foodCtrl.ingresafood = async(req,res)=>{
+foodCtrl.newDish = async (req, res) => {
     try {
         const data = req.body;
         const resp = await foodModel.create(data);
-        messageGeneral(res,201,true,resp, "Registro creado");
+        messageGeneral(res, 201, true, resp, "Registro creado");
     } catch (error) {
-        messageGeneral(res,500,false,"",error.message);
+        messageGeneral(res, 500, false, "", error.message);
     }
 };
 
 
 //Consult all records
-foodCtrl.mostrarTodo = async(req,res)=>{
+foodCtrl.listDishes = async (req, res) => {
     try {
         const resp = await foodModel.find().populate({
-            path: "folio"})
-            messageGeneral(res,)
+            path: "name"
+        });
+        messageGeneral(res,);
     } catch (error) {
-        messageGeneral(res,500,false,"",error.message);
+        messageGeneral(res, 500, false, "", error.message);
     }
 }
 
 //Update 
-foodCtrl.editarVino = async(req,res)=>{
+foodCtrl.updateDish = async (req, res) => {
     try {
-        const { id } = req.params;
-        const resp = await foodModel.findById(id);
-        if(!resp){
-            return messageGeneral(res,404,false,"","Registro no encontrada");
+        const { name } = req.params;
+        const resp = await foodModel.findById(name);
+        if (!resp) {
+            return messageGeneral(res, 404, false, "", "Registro no encontrada");
         }
         await resp.updateOne(req.body);
-        messageGeneral(res,200,true,"","Registro actualizada");
+        messageGeneral(res, 200, true, "", "Registro actualizada");
     } catch (error) {
-        messageGeneral(res,500,false,"",error.message);
+        messageGeneral(res, 500, false, "", error.message);
     }
 }
 
 //Delete
-foodCtrl.eliminarVino = async(req,res) =>{
+foodCtrl.deleteDish = async (req, res) => {
     try {
-        const { id } = req.params;
-        const resp = await foodModel.findById(id);
-        if(!resp){
-            return messageGeneral(res,404,false,"","Registro no encontrado");
+        const { name } = req.params;
+        const resp = await foodModel.findById(name);
+        if (!resp) {
+            return messageGeneral(res, 404, false, "", "Registro no encontrado");
         }
         await resp.deleteOne();
-        messageGeneral(res,200,true,"","Registro eliminado");
+        messageGeneral(res, 200, true, "", "Registro eliminado");
     } catch (error) {
-        messageGeneral(res,500,false,"",error.message);
+        messageGeneral(res, 500, false, "", error.message);
     }
 };
 
 //Consult by product name
-foodCtrl.listByName = async(req,res) => {
+foodCtrl.getDish = async (req, res) => {
     try {
         const { name } = req.params;
         const resp = await foodModel.findOne({ name });
-        if(!resp){
-            return messageGeneral(res,404,false,"","Registro no encontrado");
+        if (!resp) {
+            return messageGeneral(res, 404, false, "", "Registro no encontrado");
         }
-        messageGeneral(res,200,true,resp,"Registro encontrado")
+        messageGeneral(res, 200, true, resp, "Registro encontrado")
     } catch (error) {
-        messageGeneral(res,500,false,"",error.message);
+        messageGeneral(res, 500, false, "", error.message);
     }
 }
+
+module.exports = foodCtrl;
