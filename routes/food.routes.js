3ec5221c18e58494aa09eda979b07666e94c2fbe
MelaniRuvalcaba <melanilizethgomezruvalcaba@gmail.com>
2024-07-04 17:44:38 -0600

    feature: Nuevos epoints creados

--- routes/food.routes.js
+++ routes/food.routes.js
@@ -3,11 +3,22 @@ const controller = require("../controllers/food.controllers.js");
 
 const router = express.Router();
 
+//nombre
 router.get("/", controller.listDishes);
-router.get("/get", controller.getDish);
+router.get("/get/by-name", controller.getDishesByName);
 router.post("/new", controller.newDish);
-router.put("/update", controller.updateDish);
-router.delete("/delete", controller.deleteDish);
+router.put("/update/by-name", controller.updateDishesByName);
+router.delete("/delete/by-name", controller.deleteDishesByName);
+
+//costo
+router.get("/get/by-cost", controller.getDishesByCost);
+router.put("/update/by-cost", controller.updateDishesByCost);
+router.delete("/delete/by-cost", controller.deleteDishesByCost);
+
+//ingredientes
+router.get("/get/by-ingredients", controller.getDishesByIngredients);
+router.put("/update/by-ingredients", controller.updateDishesByIngredients);
+router.delete("/delete/by-ingredients", controller.deleteDishesByIngredients);
 
 // == == == THIS IS CALLED WHITE LIST == == ==
 router.use((req, res) => {
